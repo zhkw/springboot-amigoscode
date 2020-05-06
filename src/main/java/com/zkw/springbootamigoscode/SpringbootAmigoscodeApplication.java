3524aefe4f13f51f731162daa583205ea58a9dff
zhkw <13910066913@163.com>
2020-05-06 20:09:41 +0800

    first commit

---
+++ src/main/java/com/zkw/springbootamigoscode/SpringbootAmigoscodeApplication.java
@@ -0,0 +1,14 @@
+package com.zkw.springbootamigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAmigoscodeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootAmigoscodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zkw/springbootamigoscode/SpringbootAmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkw.springbootamigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAmigoscodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
